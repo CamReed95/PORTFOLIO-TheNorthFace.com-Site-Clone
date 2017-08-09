8b8084b24bba1d5c9ec8c52632a30ce52dbf28c2
Dave Guymon <davidwguymon@gmail.com>
2017-08-09 12:01:09 -0600

    Completed color picker functionality in iPhone6 version of DetailedView component.

--- src/components/DetailedView/DetailedView.js
+++ src/components/DetailedView/DetailedView.js
@@ -7,7 +7,8 @@ class DetailedView extends Component {
   constructor(props){
     super(props);
     this.state = {
-      product: {}
+      product: {},
+      displayImg: ''
     }
   }
 
@@ -15,19 +16,42 @@ componentDidMount() {
   console.log("DETAILED VIEW MOUNTED")
 
 getProductById(this.props.match.params.product_id).then( product => {
-  this.setState({product: product[0]})
+  this.setState({product: product[0], displayImg: product[0].img1 })
   console.log(this.state.product);
   });
 }
 
 
   render() {
+
     return (
       <div className="detailedViewContainer">
-        <h1>{this.state.product.name}</h1>
-        <p>${this.state.product.price}.00</p>
+        <div className="detailProductInfo">
+          <h1>{this.state.product.name}</h1>
+          <h1>${this.state.product.price}.00</h1>
+        </div>
 
-        <img src={this.state.product.img1} />
+        <img src={this.state.displayImg} className="detailImg" alt="product"/>
+
+        <div className="detailThumbnails">
+        {this.state.product.img1 ? <img src={this.state.product.img1} alt="product thumbnail" onClick={ ()=> {
+          this.setState({ displayImg: this.state.product.img1 })
+        }}/> :''}
+
+        {this.state.product.img2 ? <img src={this.state.product.img2} alt="product thumbnail" onClick={ ()=> {
+          this.setState({ displayImg: this.state.product.img2 })
+        }}/> :''}
+
+        {this.state.product.img3 ? <img src={this.state.product.img3} alt="product thumbnail" onClick={ ()=> {
+          this.setState({ displayImg: this.state.product.img3 })
+        }}/> :''}
+
+        {this.state.product.img4 ? <img src={this.state.product.img4} alt="product thumbnail" onClick={ ()=> {
+          this.setState({ displayImg: this.state.product.img4 })
+        }}/> :''}
+
+
+        </div>
 
       </div>
     )

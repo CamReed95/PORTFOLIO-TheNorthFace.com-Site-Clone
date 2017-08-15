0139fbea0f796d1f831ed07544ac192a63269ef3
Dave Guymon <davidwguymon@gmail.com>
2017-08-15 10:47:00 -0600

    Incorporated select dropdown in IndividualCartItem component to allow user to update quantities of items.

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -13,18 +13,33 @@ class Cart extends Component {
     const cartItems = this.props.cart.map( (item, index, arr)=> {
       return (
         <div key={index}>
-          <IndividualCartItem item={item} index={index}/>
+          <IndividualCartItem item={item} index={index} />
         </div>
       )
     })
 
     return (
       <div className="cartComponentContainer">
+        <div className="cartCountHeader">
+          {!this.props.cart.length ? <p>0 ITEMS</p> : this.props.cart.length === 1 ? <p>1 ITEM</p> : <p>{this.props.cart.length} ITEMS</p>}
+          <h2 className="inCartText">SHOPPING CART</h2>
+        </div>
+
+        <div className="checkoutContainer">
+          <div className="totalContainer">
+            <div className="orderTotal">
+              <p>ORDER TOTAL</p>
+              <p className="beforeTax">(Before Tax)</p>
+            </div>
+            <div className="numericalTotal">
+              <p>$ ** .00</p>
+            </div>
+          </div>
+        </div>
+
         <div className="cartContentsContainer">
-          <div className="cartCountMessage">
-          {!this.props.cart.length ? <p>THERE ARE NO ITEMS IN YOUR CART</p> : this.props.cart.length === 1 ? <p>1 ITEM IN YOUR CART</p> : <p>{this.props.cart.length} ITEMS IN YOUR CART</p>}
-
-
+          <div className="cartCountSubHeader">
+            {!this.props.cart.length ? <p>THERE ARE NO ITEMS IN YOUR CART</p> : this.props.cart.length === 1 ? <p>1 ITEM IN YOUR CART</p> : <p>{this.props.cart.length} ITEMS IN YOUR CART</p>}
           </div>
           {cartItems}
         </div>
--- src/components/IndividualCartItem/IndividualCartItem.js
+++ src/components/IndividualCartItem/IndividualCartItem.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
 import './individualCartItem.css';
 import { removeFromCart } from './../../ducks/reducer';
-import { Dropdown } from 'semantic-ui-react'
+import Select from 'react-select';
+import 'react-select/dist/react-select.css';
 import { connect } from 'react-redux';
 
+
 class IndividualCartItem extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+      quantity: 1
+    }
+
+    this.logQtyChange = this.logQtyChange.bind(this)
+  }
+
+
+logQtyChange(event){
+  console.log(event)
+  this.setState({
+    quantity: event.value
+  })
+}
 
   render(){
+
+    const options = [ {value: 1, label: 1}, {value: 2, label: 2}, {value: 3, label: 3}, {value: 4, label: 4}, {value: 5, label: 5}, {value: 6, label: 6}, {value: 7, label: 7}, {value: 8, label: 8}, {value: 9, label: 9} ];
+
     return(
       <div className="cartItemContainer">
         <div className="cartItemSections">
@@ -20,10 +41,17 @@ class IndividualCartItem extends Component{
             <p className="cartItemColor">{this.props.item.displayImgColor}</p>
             <p className="cartItemSize">Size - {this.props.item.selectedSize}</p>
             <p className="availableNow">Available Now to Ship</p>
+
             <div className="qtyPrice">
-              
+              <Select
+              className="qtySelector"
+              value={this.state.quantity}
+              options={options}
+              onChange={(e)=> this.logQtyChange(e)} />
+
             <p className="cartItemPrice">${this.props.item.product.price}.00</p>
             </div>
+
           </section>
           <div className="removeButton" onClick={()=> this.props.removeFromCart(this.props.index)}>
             <h1 className="removeItemLink">REMOVE</h1>
